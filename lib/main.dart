6054e53a336b61d1435c58741d97fae6569aa897
fanfaner1207 <jk22145976@gmail.com>
2023-08-01 00:50:34 +0800

    version 1.2.2 將addHistory()由_AstrologyState移出來並更名為randomList

--- lib/main.dart
+++ lib/main.dart
@@ -28,14 +28,9 @@ class Mystatefulwidget extends StatefulWidget {
 class _MystatefulwidgetState extends State<Mystatefulwidget> {
   int _setectedIndex = 0;
 
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-
   static const List _widgetOptions = [
     Astrology(),
-    Text('塔羅', style: optionStyle),
     Badfriend(),
-    // Webrtc()
   ];
 
   @override
@@ -49,9 +44,7 @@ class _MystatefulwidgetState extends State<Mystatefulwidget> {
       bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(icon: Icon(Icons.star), label: '占星'),
-            BottomNavigationBarItem(icon: Icon(Icons.star), label: '塔羅'),
             BottomNavigationBarItem(icon: Icon(Icons.star), label: '月曆'),
-            // BottomNavigationBarItem(icon: Icon(Icons.star), label: '視訊'),
           ],
           currentIndex: _setectedIndex,
           selectedItemColor: const Color.fromRGBO(234, 100, 163, 1),
--- lib/tabs/astrology.dart
+++ lib/tabs/astrology.dart
@@ -9,9 +9,10 @@ import '../data/constants.dart';
 // 中間的字體變大、變成一個表 V
 // 要有時間 V
 // 可以刪除
-// 永久記得
+// 永久記得->DB  https://juejin.cn/post/7150064694584475656
 // 針對每行可以手動備註
 // Datatable要修改文字大小
+// AppBar上下有點粗
 
 BottomDrawerController controller = BottomDrawerController();
 Widget buildBottomDrawer(BuildContext context) {
@@ -19,8 +20,7 @@ Widget buildBottomDrawer(BuildContext context) {
     header: const Center(
         child: Text(
       "歷史紀錄",
-      style: TextStyle(
-          fontFamily: "NotoSansTC", fontSize: 18, fontWeight: FontWeight.w500),
+      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
     )),
 
     body: ListView.builder(
@@ -52,15 +52,25 @@ class Astrology extends StatefulWidget {
   State<Astrology> createState() => _AstrologyState();
 }
 
+List randomList({int house = 0, int planetNum = 10, int starSignNum = 12}) {
+  String time = DateTime.now().toString().split('.')[0];
+  house = Random(DateTime.now().millisecondsSinceEpoch).nextInt(12) + 1; //宮位
+  planetNum = Random(DateTime.now().millisecondsSinceEpoch).nextInt(10);
+  starSignNum = Random(DateTime.now().millisecondsSinceEpoch).nextInt(12);
+  return [
+    time,
+    starSign[starSignNum],
+    '${house.toString()}宮',
+    planet[planetNum]
+  ];
+}
+
 class _AstrologyState extends State<Astrology> {
-  void addHistory() {
-    String time = DateTime.now().toString().split('.')[0];
-    _house = Random(DateTime.now().millisecondsSinceEpoch).nextInt(12) + 1; //宮位
-    _planetNum = Random(DateTime.now().millisecondsSinceEpoch).nextInt(10);
-    _starSignNum = Random(DateTime.now().millisecondsSinceEpoch).nextInt(12);
+  List tmp = ['NA', 'NA', 'NA', 'NA'];
+  void addHistoryList() {
     setState(() {
-      history.add(
-          '$time\t\t${starSign[_starSignNum]}\t\t$_house宮\t\t${planet[_planetNum]}');
+      tmp = randomList();
+      history.add('${tmp[0]}\t\t${tmp[1]}\t\t${tmp[2]}\t\t${tmp[3]}');
     });
     if (history[0] == "目前沒有喔") {
       history.removeAt(0);
@@ -70,9 +80,9 @@ class _AstrologyState extends State<Astrology> {
     }
   }
 
-  int _starSignNum = 12;
-  int _planetNum = 10;
-  int _house = 0;
+  // int _starSignNum = 12;
+  // int _planetNum = 10;
+  // int _house = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -86,7 +96,6 @@ class _AstrologyState extends State<Astrology> {
                         color: Colors.grey)
                   ]))),
           backgroundColor: const Color.fromARGB(255, 255, 192, 209),
-          // titleTextStyle:
         ),
         body: Stack(
           children: [
@@ -94,23 +103,31 @@ class _AstrologyState extends State<Astrology> {
                 child: Column(
               mainAxisSize: MainAxisSize.max,
               children: [
-                DataTable(columns: const [
-                  DataColumn(label: Text('項目')),
-                  DataColumn(label: Text('抽取'))
-                ], rows: [
-                  DataRow(cells: [
-                    const DataCell(Text('宮位')),
-                    DataCell(Text('$_house宮'))
-                  ]),
-                  DataRow(cells: [
-                    const DataCell(Text('守護星')),
-                    DataCell(Text('${planet[_planetNum]}'))
-                  ]),
-                  DataRow(cells: [
-                    const DataCell(Text('星座')),
-                    DataCell(Text('${starSign[_starSignNum]}'))
-                  ]),
-                ])
+                DataTable(
+                    columnSpacing: 30,
+                    headingRowHeight: 40,
+                    dataRowMinHeight: 40,
+                    // headingTextStyle: const TextStyle(fontSize: 18),
+                    // dataTextStyle: const TextStyle(fontSize: 18),
+                    columns: const [
+                      DataColumn(label: Text('項目')),
+                      DataColumn(label: Text('抽取'))
+                    ],
+                    rows: [
+                      DataRow(cells: [
+                        const DataCell(Text('守護星')),
+                        // DataCell(Text('${tmp[3]}'))
+                        DataCell(Text('${tmp[3]}'))
+                      ]),
+                      DataRow(cells: [
+                        const DataCell(Text('宮位')),
+                        DataCell(Text('${tmp[2]}'))
+                      ]),
+                      DataRow(cells: [
+                        const DataCell(Text('星座')),
+                        DataCell(Text('${tmp[1]}'))
+                      ]),
+                    ])
               ],
             )),
             buildBottomDrawer(context)
@@ -121,7 +138,7 @@ class _AstrologyState extends State<Astrology> {
           label: const Text('抽'),
           tooltip: '無情開抽',
           onPressed: () => setState(() {
-            addHistory();
+            addHistoryList();
             controller.open();
           }),
         ));
